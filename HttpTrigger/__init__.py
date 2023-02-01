c411a11cd0460bb97b06b19d447d99111a220f95
JoshuaMS356 <77986656+JoshuaMS356@users.noreply.github.com>
2023-02-01 08:55:13 -0600

    Update __init__.py

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -1,40 +1,43 @@
 import logging
-
+import time
+from multiprocessing.connection import wait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import azure.functions as func
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from azure.identity import DefaultAzureCredential, ClientSecretCredential
-from azure.storage.blob import BlobServiceClient
 from datetime import datetime
 import os
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request. 11:38')
-
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-
     driver = webdriver.Chrome("/usr/local/bin/chromedriver", chrome_options=chrome_options)
-    driver.get('http://www.ubuntu.com/')
-    links = driver.find_elements(By.TAG_NAME, "a")
-    link_list = ""
-    for link in links:
-        if link_list == "":
-            link_list = link.text
-        else:
-            link_list = link_list + ", " + link.text
-
-    # create blob service client and container client
-    credential = DefaultAzureCredential()
-    storage_account_url = "https://" + os.environ["par_storage_account_name"] + ".blob.core.windows.net"
-    client = BlobServiceClient(account_url=storage_account_url, credential=credential)
-    blob_name = "test" + str(datetime.now()) + ".txt"
-    blob_client = client.get_blob_client(container=os.environ["par_storage_container_name"], blob=blob_name)
-    blob_client.upload_blob(link_list)
-
+    
+    driver.get('https://www.donotcall.gov/register.html')
+    links = driver.current_url
+    
+    #STEP 1 OF REGISTRATION SUCCESSFUL
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_Button1"))).click()
+   
+    #STEP 2 OF REGISTRATION SUCCESSFUL
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_PhoneNumberTextBox1"))).send_keys("3013556623")
+    link = driver.current_url
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_EmailAddressTextBox"))).click()
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_EmailAddressTextBox"))).send_keys("test@gmail.com")    
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_ConfirmEmailAddressTextBox"))).send_keys("test@gmail.com")
+    driver.find_element_by_id("ContentPlaceHolder1_SubmitButton1").click()
+    ulink = driver.current_url
+    
+    #STEP 3 OF REGISTRATION SUCCESSFUL
+    logging.info('Currently on step ' + ulink)
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "ContentPlaceHolder1_RegisterButton"))).click()
+    time.sleep(2)
+    plink = driver.current_url
+    logging.info('Currently on step ' + plink)
     return func.HttpResponse(
-             str(link_list),
+             str(links + '  ' + link + '  ' + ulink + '  ' + plink),
              status_code=200
     )
